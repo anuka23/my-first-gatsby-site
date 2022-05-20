08549708f10707b4574a86cbda6472074c41206b
Anushka Vyas <anushkavyas98@gmail.com>
2022-05-20 09:51:24 +0530

    Finished Gatsby Tutorial Part 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import girls from "../images/cat1.jpg";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <img src={girls} alt="Girls" style={{ width: "100%" }} />
     </Layout>
   );
 };
